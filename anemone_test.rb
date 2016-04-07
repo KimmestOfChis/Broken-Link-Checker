ab10e0b42e56ff30dedb0c4a1690a4e3a3a0439e
Matthew Johnson <memjay3279@gmail.com>
2016-04-07 16:22:10 -0400

    Comments added

--- anemone_test.rb
+++ anemone_test.rb
@@ -4,19 +4,19 @@ require 'uri'
 pager = []
 
 startTime = Time.now
-  	puts "Test started at" + startTime.to_s
+  	puts "Test started at" + startTime.to_s #Start time
 
-Anemone.crawl("http://www.trektoday.com/" , :depth_limit => 1) do |anemone|
+
+Anemone.crawl("http://www.trektoday.com/" , :depth_limit => 3) do |anemone|
   anemone.on_every_page do |page|
   	puts page.url
- 
 pager.push(page.url)
 end
 end
 
 puts "Test completed at #{Time.now}"
   	totalTime = Time.now - startTime
-  	puts "Total elapsed for test: #{totalTime} seconds."
+  	puts "Total elapsed for test: #{totalTime} seconds." #Start Time in Minutes
 
 urlSet = pager.join(' ')
 fname = 'urlFiles.txt'
