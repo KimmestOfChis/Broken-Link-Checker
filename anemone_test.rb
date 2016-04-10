53b9ca3b8012ec82ca9f58b3a250f872a6fb6783
Matthew Johnson <memjay3279@gmail.com>
2016-04-10 17:20:57 -0400

    Correctable User input added

--- anemone_test.rb
+++ anemone_test.rb
@@ -3,11 +3,25 @@ require 'uri'
 
 pager = []
 
+puts "What site would you like to search?" #add future regex corrections, and REP obeyance 
+$webSite = gets.chomp
+
+$webSite.strip()
+
+$webSite = "http://www.#{$webSite}.com"
+
+
+
+puts "What depth do you want to set this crawler to?"
+depth = gets.chomp
+
+$setDepth = depth.to_i
+
 startTime = Time.now
-  	puts "Test started at" + startTime.to_s #Start time
+  	puts "Test started at " + startTime.to_s #Start time
 
 
-Anemone.crawl("http://www.trektoday.com/" , :depth_limit => 3) do |anemone|
+Anemone.crawl($webSite , :depth_limit => $setDepth) do |anemone|
   anemone.on_every_page do |page|
   	puts page.url
 pager.push(page.url)
@@ -15,8 +29,14 @@ end
 end
 
 puts "Test completed at #{Time.now}"
-  	totalTime = Time.now - startTime
-  	puts "Total elapsed for test: #{totalTime} seconds." #Start Time in Minutes
+totalTime = Time.now - startTime
+if totalTime <= 60
+	puts "#{totalTime} seconds elapsed for test."
+else
+puts "Total elapsed for test: #{totalTime.to_i/60} minutes."
+end
+
+puts "#{pager.length.to_s} pages indexed."
 
 urlSet = pager.join(' ')
 fname = 'urlFiles.txt'
