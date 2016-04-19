2a6a087f3e08ab9d7dc7988f370d80a516e67eb8
Matthew Johnson <memjay3279@gmail.com>
2016-04-19 18:37:51 -0400

    Demonstration of Anemone for teaching

--- anemone_test.rb
+++ anemone_test.rb
@@ -1,17 +1,11 @@
-require './404_finder.rb'
 require 'anemone'
-require 'uri'
 
 
-pager = []
 
-puts "What site would you like to search?" #add future regex corrections, and REP obeyance 
+puts "What site would you like to search?"
 $webSite = gets.chomp
 
-$webSite.strip()
-
 $webSite = "http://www.#{$webSite}"
-
 
 
 puts "What depth do you want to set this crawler to?"
@@ -28,33 +22,4 @@ Anemone.crawl($webSite , :depth_limit => $setDepth, :obey_robots_txt => true, :t
   	puts page.url
 pager.push(page.url)
 end
-end
-
-puts "Test completed at #{Time.now}"
-totalTime = Time.now - startTime
-if totalTime <= 60
-	puts "#{totalTime} seconds elapsed for test."
-else
-puts "Total elapsed for test: #{totalTime.to_i/60} minutes."
-end
-
-puts "#{pager.length.to_s} pages indexed."
-
-urlSet = pager.join(' ')
-fname = 'urlFiles.txt'
-filer = File.open(fname, 'w')
-filer.puts urlSet
-filer.close
-
-puts "Would you like to check indexed pages for response codes and broken links?"
-answer = gets.chomp
-
-case answer
-when "yes", "Yes", "YES", "y", "Y"
-	BrokenLinkChecker.new
-when "No", "no", "NO", "n", "N"
-	puts "Program complete."
-	abort()
-else
-	puts "Please enter yes or no"
 end
